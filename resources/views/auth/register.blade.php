55713ed771632aabbba6f5ce49bfc33534321f85
AdrianAmenKoSoroka <xsorokaa@stuba.sk>
2023-05-07 11:09:24 +0200

    Added Role management to register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,6 +16,13 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
+        <!-- Role Selection -->
+        <div class="mt-4">
+            <x-input-label for="role" :value="__('Role')" />
+            <x-text-input id="role" class="block mt-1 w-full" type="text" name="role" :value="old('role')" required autocomplete="role" />
+            <x-input-error :messages="$errors->get('role')" class="mt-2" />
+        </div>
+
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
