06532479675929e7f819d7d14ee4c24afc4fdb7e
peter <xkopecky@stuba.sk>
2023-05-11 20:44:06 +0200

    test commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,6 +55,8 @@
                                 </p>
                             </div>
 
+                            <p>hello</p>
+
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
                             </svg>
