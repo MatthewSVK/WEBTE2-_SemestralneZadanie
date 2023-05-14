b8f000099106547749b15adf7ee48c75d0cd480b
peter <xkopecky@stuba.sk>
2023-05-14 18:28:35 +0200

    + pridana tabulka studentov + datatable + upravy v preklade

--- lang/en/normal.php
+++ lang/en/normal.php
@@ -2,5 +2,12 @@
 
 return [
     'hello' => "MathPac - home page",
+    'teacher-page' => "MathPac - teacher portal",
+    'teacher-page-title' => "Files settings",
+    'teacher-page-desc-1' => "In the following form, you have the opportunity to define which latex files you will be 
+                able to use the student to generate examples for solving and in which period he will be able to 
+                generate them.",
+    'teacher-page-desc-2' => "The following table contains an overview of students with information on how many 
+                tasks each student generated, how many they submitted and how many points they earned.",
     'langSelect' => "Language selection"
 ];
--- lang/sk/normal.php
+++ lang/sk/normal.php
@@ -2,5 +2,11 @@
 
 return [
     'hello' => "MathPac - úvodná stránka",
+    'teacher-page' => "MathPac - učíteľský portál",
+    'teacher-page-title' => "Nastavenia súborov",
+    'teacher-page-desc-1' => "V nasledujúcom formulári máte možnosť  definovať,  z  ktorých  latexových  súborov  si  bude  môcť
+                študent generovať príklady na riešenie a v ktorom období si ich bude môcť generovať.",
+    'teacher-page-desc-2' => "V nasledujúcej tabuľke je prehľad študentov s informáciou, koľko úloh si ktorý študent 
+                vygeneroval, koľko ich odovzdal a koľko za ne získal bodov.",
     'langSelect' => "Výber jazyku"
 ];
