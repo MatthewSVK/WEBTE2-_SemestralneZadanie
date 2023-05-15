3e43cbeedd54fe79ceed165298ad0f810b59e0b2
peter <xkopecky@stuba.sk>
2023-05-15 13:23:37 +0200

    + upravy hlavnej stranky

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,30 +23,40 @@
 @endsection
 
 @section('content')
-    <h1 class="text-uppercase fs-1 fw-bold mt-5">{{__('normal.home-page-title')}}</h1>
+    <header class="mt-5 mb-5">
+        <h1 class="text-uppercase fs-1 fw-bold">{{__('normal.home-page-title')}}</h1>
+    </header>
 
-    <p>
-        {{__('normal.home-page-desc1')}}
-    </p>
-    <ul style="text-align: left">
-        <li>
-            {{__('normal.home-page-desc2')}} <strong>{{__('normal.home-page-desc3')}}</strong>
-            {{__('normal.home-page-desc4')}}
-        </li>
-        <li>
-            {{__('normal.home-page-desc2')}} <strong>{{__('normal.home-page-desc5')}}</strong>
-            {{__('normal.home-page-desc6')}}
-        </li>
-    </ul>
-    <p>
-        {{__('normal.home-page-desc7')}}
-    </p>
+    <div class="border-1 text-white border-dark py-4 mx-auto mb-3 card  text-center"
+         style="padding: 0 2% 0 2%; max-width: 90%; background: rgba(16,16,16,0.7);">
+        <p>
+            {{__('normal.home-page-desc1')}}
+        </p>
+        <ul style="text-align: left">
+            <li>
+                {{__('normal.home-page-desc2')}} <strong>{{__('normal.home-page-desc3')}}</strong>
+                {{__('normal.home-page-desc4')}}
+            </li>
+            <li>
+                {{__('normal.home-page-desc2')}} <strong>{{__('normal.home-page-desc5')}}</strong>
+                {{__('normal.home-page-desc6')}}
+            </li>
+        </ul>
+        <p>
+            {{__('normal.home-page-desc7')}}
+        </p>
+    </div>
+    <div>
+        <button type="submit" name="instructionPDF" id="instructionPDF" class="btn mb-2 text-success"
+                style="background: rgba(16,16,16,0.7);">Download to PDF</button>
+    </div>
 
     <hr class=" mt-3 mb-3 hr hr-blurry"/>
 
-    <h2 class="text-uppercase fs-1 fw-bold mt-5">{{__('normal.home-page-title2')}}</h2>
+    <h2 class="text-uppercase fw-bold mt-3 mb-3">{{__('normal.home-page-title2')}}</h2>
 
-    <div class="text-center">
+    <div class="border-1 text-white border-dark py-4 mx-auto mb-3 card  text-center"
+         style="padding: 0 2% 0 2%; max-width: 50%; background: rgba(16,16,16,0.7);">
         *tu bude video*
     </div>
 
