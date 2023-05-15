122c0898a0417ff08346f0206be50f42056f990b
peter <xkopecky@stuba.sk>
2023-05-15 12:03:38 +0200

    + jazycnost na hlavnej stranke + vyzor

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,16 +12,42 @@
 </li>
 @endsection
 
+@section('styles')
+    <style>
+
+    </style>
+@endsection
+
 @section('page-name')
     {{__('normal.hello')}}
 @endsection
 
 @section('content')
-    <h1 class="text-uppercase fs-1 fw-bold mt-5">Úvodná stránka</h1>
+    <h1 class="text-uppercase fs-1 fw-bold mt-5">{{__('normal.home-page-title')}}</h1>
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At delectus eius reiciendis suscipit ut voluptas
-        voluptatibus? Autem blanditiis, error et natus neque nulla odio placeat quam voluptatem voluptatum. Dicta, sapiente.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias consectetur cum deleniti error eum facere in
-        ipsum libero maiores neque officia placeat, quia quis, ratione reiciendis reprehenderit tempora voluptas?
+    <p>
+        {{__('normal.home-page-desc1')}}
     </p>
+    <ul style="text-align: left">
+        <li>
+            {{__('normal.home-page-desc2')}} <strong>{{__('normal.home-page-desc3')}}</strong>
+            {{__('normal.home-page-desc4')}}
+        </li>
+        <li>
+            {{__('normal.home-page-desc2')}} <strong>{{__('normal.home-page-desc5')}}</strong>
+            {{__('normal.home-page-desc6')}}
+        </li>
+    </ul>
+    <p>
+        {{__('normal.home-page-desc7')}}
+    </p>
+
+    <hr class=" mt-3 mb-3 hr hr-blurry"/>
+
+    <h2 class="text-uppercase fs-1 fw-bold mt-5">{{__('normal.home-page-title2')}}</h2>
+
+    <div class="text-center">
+        *tu bude video*
+    </div>
+
 @endsection
